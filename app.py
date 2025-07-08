90b38e859fe4f0eb82845dc1c7613f0c197dd3c2
rajan <rajanbhateja6@gmail.com>
2025-07-08 13:16:43 +0530

    Streaming added and prompt optimized

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-import os
+import asyncio
+
 from dotenv import load_dotenv
 
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, GoogleGenerativeAI
@@ -13,7 +14,7 @@ load_dotenv()
 embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
 vector_store = FAISS.load_local("word_embeddings", embeddings, allow_dangerous_deserialization=True)
 
-llm = GoogleGenerativeAI(model="gemini-2.5-flash")
+llm = GoogleGenerativeAI(model="gemini-2.5-flash", disable_streaming=False)
 
 @cl.on_message
 async def on_chat_start(message: cl.Message):
@@ -25,13 +26,20 @@ async def on_chat_start(message: cl.Message):
     prompt = f"""
         You are a supportive self-help advisor named MindMint.
         Use the following excerpts from classic self-help books to craft a clear, practical, actionable answer in points, preferably with examples.
-        
+        If a query is asked which is not relevant to self-help, simply respond by saying it's out of my domain.
         User's question: {query}
         
         Relevant Excerpts: {context}
-        
-        Answer:
     """
 
-    response = await llm.ainvoke(prompt)
-    await cl.Message(content=response).send()
+    # Send initial message
+    response_msg = cl.Message(content="")
+    await response_msg.send()
+
+    # Stream tokens
+    async for chunk in llm.astream(prompt):
+        await response_msg.stream_token(chunk)
+        await asyncio.sleep(0.02)
+
+    # Final update
+    await response_msg.update()
